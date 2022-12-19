eef0bde3e46a07357a862aa5edea59f530cede76
keepwonder <qiangjiong1993@163.com>
2022-12-19 15:03:29 +0800

    动态生成侧边栏菜单

--- practice/book_manager/book_mgr_fe/src/layout/menu/Menu.vue
+++ practice/book_manager/book_mgr_fe/src/layout/menu/Menu.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import MenuLogoVue from './MenuLogo.vue'
+import MenuItemVue from './MenuItem.vue';
 
 </script>
 
@@ -9,54 +10,7 @@ import MenuLogoVue from './MenuLogo.vue'
     <!-- 侧边栏导航 -->
     <el-menu active-text-color="#ffd04b" background-color="#304156" class="el-menu-vertical-demo" default-active="2"
         text-color="#fff">
-        <el-sub-menu index="1">
-            <template #title>
-                <el-icon>
-                    <location />
-                </el-icon>
-                <span>图书管理</span>
-            </template>
-            <el-menu-item index="1-1">书籍管理</el-menu-item>
-            <el-menu-item index="1-2">出版社管理</el-menu-item>
-            <el-menu-item index="1-3">出品方管理</el-menu-item>
-            <el-menu-item index="1-4">作者管理</el-menu-item>
-        </el-sub-menu>
-        <el-sub-menu index="2">
-            <template #title>
-                <el-icon>
-                    <location />
-                </el-icon>
-                <span>电影管理</span>
-            </template>
-            <el-menu-item index="2-1">书籍管理</el-menu-item>
-            <el-menu-item index="2-2">出版社管理</el-menu-item>
-            <el-menu-item index="2-3">出品方管理</el-menu-item>
-            <el-menu-item index="2-4">作者管理</el-menu-item>
-        </el-sub-menu>
-        <el-sub-menu index="3">
-            <template #title>
-                <el-icon>
-                    <location />
-                </el-icon>
-                <span>音乐管理</span>
-            </template>
-            <el-menu-item index="3-1">书籍管理</el-menu-item>
-            <el-menu-item index="3-2">出版社管理</el-menu-item>
-            <el-menu-item index="3-3">出品方管理</el-menu-item>
-            <el-menu-item index="3-4">作者管理</el-menu-item>
-        </el-sub-menu>
-        <el-sub-menu index="4">
-            <template #title>
-                <el-icon>
-                    <location />
-                </el-icon>
-                <span>用户管理</span>
-            </template>
-            <el-menu-item index="4-1">登录账号</el-menu-item>
-            <el-menu-item index="4-2">用户角色</el-menu-item>
-            <el-menu-item index="4-3">菜单管理</el-menu-item>
-            <el-menu-item index="4-4">权限信息</el-menu-item>
-        </el-sub-menu>
+        <MenuItemVue></MenuItemVue>
     </el-menu>
 </template>
 
@@ -64,6 +18,7 @@ import MenuLogoVue from './MenuLogo.vue'
 .el-menu-item {
     display: block;
     text-align: center;
+    padding-right: 80px;
 }
 
 .el-menu-vertical-demo:not(.el-menu--collapse) {
---
+++ practice/book_manager/book_mgr_fe/src/layout/menu/MenuItem.vue
@@ -0,0 +1,148 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+let menuList = reactive([
+    {
+        path: '/',
+        meta: {
+            'title': '首页'
+        }
+    },
+    {
+        path: '/book_mgr',
+        meta: {
+            'title': '图书管理'
+        },
+        children: [
+            {
+                path: '/book_mgr/books',
+                meta: {
+                    'title': '书籍管理'
+                },
+            },
+            {
+                path: '/book_mgr/publishers',
+                meta: {
+                    'title': '出版社管理'
+                },
+            },
+            {
+                path: '/book_mgr/producers',
+                meta: {
+                    'title': '出品方管理'
+                },
+            },
+            {
+                path: '/book_mgr/auhtors',
+                meta: {
+                    'title': '作者管理'
+                },
+            },
+        ]
+
+    },
+    {
+        path: '/user_mgr',
+        meta: {
+            'title': '用户管理'
+        },
+        children: [
+            {
+                path: '/user_mgr/account',
+                meta: {
+                    'title': '账号管理'
+                },
+            },
+            {
+                path: '/user_mgr/role',
+                meta: {
+                    'title': '角色管理'
+                },
+            },
+            {
+                path: '/user_mgr/auhtorization',
+                meta: {
+                    'title': '权限管理'
+                },
+            },
+        ]
+
+    },
+])
+
+</script>
+
+<template>
+    <template v-for="menu in menuList">
+        <el-sub-menu v-if="menu.children && menu.children.length > 0" :index="menu.path">
+            <template #title>
+                <span style="padding-left: 40px;">{{ menu.meta.title }}</span>
+            </template>
+            <template v-for="child in menu.children">
+                <el-menu-item :index="child.path">
+                    <span>{{ child.meta.title }}</span>
+                </el-menu-item>
+            </template>
+        </el-sub-menu>
+        <el-menu-item v-else :index="menu.path"><span>{{ menu.meta.title }}</span></el-menu-item>
+    </template>
+    <!-- 
+        <el-menu-item index="0">首页</el-menu-item>
+        <el-sub-menu index="1">
+            <template #title>
+                <el-icon>
+                    <location />
+                </el-icon>
+                <span>图书管理</span>
+            </template>
+            <el-menu-item index="/books">书籍管理</el-menu-item>
+            <el-menu-item index="/publishers">出版社管理</el-menu-item>
+            <el-menu-item index="/producer">出品方管理</el-menu-item>
+            <el-menu-item index="/authors">作者管理</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="2">
+            <template #title>
+                <el-icon>
+                    <location />
+                </el-icon>
+                <span>电影管理</span>
+            </template>
+            <el-menu-item index="2-1">书籍管理</el-menu-item>
+            <el-menu-item index="2-2">出版社管理</el-menu-item>
+            <el-menu-item index="2-3">出品方管理</el-menu-item>
+            <el-menu-item index="2-4">作者管理</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="3">
+            <template #title>
+                <el-icon>
+                    <location />
+                </el-icon>
+                <span>音乐管理</span>
+            </template>
+            <el-menu-item index="3-1">书籍管理</el-menu-item>
+            <el-menu-item index="3-2">出版社管理</el-menu-item>
+            <el-menu-item index="3-3">出品方管理</el-menu-item>
+            <el-menu-item index="3-4">作者管理</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="4">
+            <template #title>
+                <el-icon>
+                    <location />
+                </el-icon>
+                <span>用户管理</span>
+            </template>
+            <el-menu-item index="4-1">登录账号</el-menu-item>
+            <el-menu-item index="4-2">用户角色</el-menu-item>
+            <el-menu-item index="4-3">菜单管理</el-menu-item>
+            <el-menu-item index="4-4">权限信息</el-menu-item>
+        </el-sub-menu>
+ -->
+</template>
+
+<style scoped>
+.el-menu-item {
+  display: block;
+  text-align: center;
+  padding-right: 80px;
+}
+</style>
